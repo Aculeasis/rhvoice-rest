fd37eb0c2e2635a11da3bf6f44370f945815cb3d
aculeasis <42580940+Aculeasis@users.noreply.github.com>
2018-10-03 10:25:16 +0300

    Adopt multiprocessing work for asyncio
    Not profitable, flask faster

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import asyncio
+import threading
 from shlex import quote
 from urllib import parse
 
@@ -24,7 +26,6 @@ DEFAULT_FORMAT = 'mp3'
 
 
 async def say(request):
-    print(request)
     text = ' '.join([x for x in parse.unquote(request.rel_url.query.get('text', '')).splitlines() if x])
     voice = request.rel_url.query.get('voice', DEFAULT_VOICE)
     format_ = request.rel_url.query.get('format', DEFAULT_FORMAT)
@@ -44,13 +45,64 @@ async def say(request):
     response.content_type = FORMATS[format_]
     await response.prepare(request)
     try:
-        with tts.say(text, voice, format_) as read:
-            for chunk in read:
-                await response.write(chunk)
+        if tts.process:
+            with AsyncAdapter(tts.say, text, voice, format_) as read:
+                async for chunk in read:
+                    await response.write(chunk)
+        else:
+            with tts.say(text, voice, format_) as read:
+                for chunk in read:
+                    await response.write(chunk)
     except ConnectionResetError as e:
+        print('{} "{} {} HTTP/{}.{}"'.format(request.remote, request.method, request.path_qs, *request.version))
         print(e)
     else:
         return response
+
+
+class AsyncAdapter(threading.Thread):
+    def __init__(self, say_func, *params):
+        super().__init__()
+        self._say = say_func
+        self._buff = None
+        self._wait = threading.Event()
+        self._work = False
+        self._params = params
+
+    def run(self):
+        with self._say(*self._params) as read:
+            for chunk in read:
+                self._buff = chunk
+                self._wait.wait()
+                if self._buff is not None or not self._work:
+                    break
+                self._wait.clear()
+        self._work = False
+
+    def __aiter__(self):
+        return self
+
+    def __enter__(self):
+        self._work = True
+        self.start()
+        return self
+
+    def __exit__(self, *_):
+        if self._work:
+            self._work = False
+            self._wait.set()
+            self.join()
+
+    async def __anext__(self):
+        while self._buff is None:
+            if not self._work:
+                raise StopAsyncIteration
+            await asyncio.sleep(0)
+        try:
+            return self._buff
+        finally:
+            self._buff = None
+            self._wait.set()
 
 
 def _get_def(any_, test):
